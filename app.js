0c7e5e87c0c29eb89ce30f1c97b3d9bb0a5401d6
Abhishek singh <abhisheksingh6716@gmail.com>
2021-05-14 06:58:56 +0530

    changes js logic

--- app.js
+++ app.js
@@ -8,32 +8,44 @@ let flag = true;
 function startTimer() {
     if (flag === true) {
         flag = false;
-
-        if (flag === false) {
-            sec = parseInt(sec);
-            min = parseInt(min);
-            hr = parseInt(hr);
-
-            sec = sec + 1;
-
-            if (sec === 60) {
-                min = min + 1;
-                sec = 0;
-            }
-            if (min === 60) {
-                hr = hr + 1;
-                min = 0;
-                sec = 0;
-            }
-
-            timer.innerHTML = hr + ':' + min + ':' + sec;
-            setTimeout("timerCycle()", 1000);
-        }
+        cycle();
     }
 }
 function stopTimer() {
     if (flag === false) {
         flag = true;
+    }
+}
+
+function cycle() {
+    if (flag === false) {
+        sec = parseInt(sec);
+        min = parseInt(min);
+        hr = parseInt(hr);
+
+        sec = sec + 1;
+
+        if (sec === 60) {
+            min = min + 1;
+            sec = 0;
+        }
+        if (min === 60) {
+            hr = hr + 1;
+            min = 0;
+            sec = 0;
+        }
+        if (sec < 10 || sec === 0) {
+            sec = '0' + sec;
+        }
+        if (min < 10 || min === 0) {
+            min = '0' + min;
+        }
+        if (hr < 10 || hr === 0) {
+            hr = '0' + hr;
+        }
+
+        timer.innerHTML = hr + ':' + min + ':' + sec;
+        setTimeout("cycle()", 1000);
     }
 }
 
