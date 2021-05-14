aa8db97af0625775abedfe8dfca690ac4274e2d8
Abhishek singh <abhisheksingh6716@gmail.com>
2021-05-14 06:45:53 +0530

    changes js logic

--- app.js
+++ app.js
@@ -3,55 +3,52 @@ const timer = document.getElementById('stopwatch');
 let hr = 0;
 let min = 0;
 let sec = 0;
-let stoptime = true;
+let flag = true;
 
 function startTimer() {
-    if (stoptime === true) {
-        stoptime = false;
-        timerCycle();
+    if (flag === true) {
+        flag = false;
+
+        if (flag === false) {
+            sec = parseInt(sec);
+            min = parseInt(min);
+            hr = parseInt(hr);
+
+            sec = sec + 1;
+
+            if (sec === 60) {
+                min = min + 1;
+                sec = 0;
+            }
+            if (min === 60) {
+                hr = hr + 1;
+                min = 0;
+                sec = 0;
+            }
+            if (sec < 10 || sec === 0) {
+                sec = '0' + sec;
+            }
+            if (min < 10 || min === 0) {
+                min = '0' + min;
+            }
+            if (hr < 10 || hr === 0) {
+                hr = '0' + hr;
+            }
+
+            timer.innerHTML = hr + ':' + min + ':' + sec;
+            setTimeout("timerCycle()", 1000);
+        }
     }
 }
 function stopTimer() {
-    if (stoptime === false) {
-        stoptime = true;
-    }
-}
-
-function timerCycle() {
-    if (stoptime === false) {
-        sec = parseInt(sec);
-        min = parseInt(min);
-        hr = parseInt(hr);
-
-        sec = sec + 1;
-
-        if (sec === 60) {
-            min = min + 1;
-            sec = 0;
-        }
-        if (min === 60) {
-            hr = hr + 1;
-            min = 0;
-            sec = 0;
-        }
-        if (sec < 10 || sec === 0) {
-            sec = '0' + sec;
-        }
-        if (min < 10 || min === 0) {
-            min = '0' + min;
-        }
-        if (hr < 10 || hr === 0) {
-            hr = '0' + hr;
-        }
-
-        timer.innerHTML = hr + ':' + min + ':' + sec;
-        setTimeout("timerCycle()", 1000);
+    if (flag === false) {
+        flag = true;
     }
 }
 
 function resetTimer() {
     timer.innerHTML = "00:00:00";
-    stoptime = true;
+    flag = true;
     hr = 0;
     sec = 0;
     min = 0;
